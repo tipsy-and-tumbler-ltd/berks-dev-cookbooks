75f50dd836cc41bebd0dbdb087c64c9658ad836f
Daniel Poulin <dan.poulin@digitalsurgeons.com>
2013-05-24 09:38:39 -0400

    Clean up the output of the script a little.

--- install-dnsmasq.sh
+++ install-dnsmasq.sh
@@ -7,14 +7,14 @@ if [ ! $1 ]; then
 fi
 
 # Check for existence/accessibility of mysql and sed
-echo "Checking dependencies.."
+echo ":: Checking dependencies.."
 hash brew 2>&- || { echo >&2 "Homebrew is required but is either not installed or not in your PATH.  Aborting."; exit 1; }
 
 # Install dnsmasq
-echo "Installing dnsmasq with homebrew.."
+echo ":: Installing dnsmasq with homebrew.."
 brew install dnsmasq
 
-echo "Configuring dnsmasq.."
+echo ":: Configuring dnsmasq.."
 # Create config file
 cp /usr/local/opt/dnsmasq/dnsmasq.conf.example /usr/local/etc/dnsmasq.conf
 echo "address=/$1.dsdev/33.33.33.10" >> /usr/local/etc/dnsmasq.conf
@@ -24,10 +24,13 @@ sudo touch /etc/resolver/dsdev
 sudo echo "nameserver 127.0.0.1" > /etc/resolver/dsdev
 
 # Make it load at start
-echo "Registering dnsmasq as a startup daemon..
+echo ":: Registering dnsmasq as a startup daemon..
 Your sudo password will be required."
 sudo cp -fv /usr/local/opt/dnsmasq/*.plist /Library/LaunchDaemons
 sudo launchctl load /Library/LaunchDaemons/homebrew.mxcl.dnsmasq.plist
 
 
-echo "You're all set! *.$1.dsdev will now point to your vagrant machine at 33.33.33.10. Edit /usr/local/etc/dnsmasq.conf if you need to make configuration adjustments."
+echo ":: You're all set!
+
+*.$1.dsdev will now point to your vagrant machine at 33.33.33.10.
+Edit /usr/local/etc/dnsmasq.conf if you need to make configuration adjustments."
