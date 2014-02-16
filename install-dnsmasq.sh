6528e5d7b08b3ee0bc9f9890baca32fa770aa9d5
Mhd Zaher Ghaibeh <linux.juggler@gmail.com>
2014-02-16 17:13:45 +0200

    Update install-dnsmasq.sh
    changed the place of dnsmasq (/usr/local/Cellar/dnsmasq/2.68/dnsmasq.conf.example)

--- install-dnsmasq.sh
+++ install-dnsmasq.sh
@@ -2,7 +2,7 @@
 
 # Check that user is asking for an action
 if [ ! $1 ]; then
-	echo >&2 "Pass the initials you want for your localhost address, ex: *.<your-initials>.dsdev."
+	echo >&2 "Pass the initials you want for your localhost address, ex: *.<your-initials>.dev."
   	exit 1;
 fi
 if [ ! $2 ]; then
@@ -20,17 +20,18 @@ brew install dnsmasq
 
 echo ":: Configuring dnsmasq.."
 # Create config file
-cp /usr/local/opt/dnsmasq/dnsmasq.conf.example /usr/local/etc/dnsmasq.conf
-echo "address=/$1.dsdev/$2" >> /usr/local/etc/dnsmasq.conf
+cp /usr/local/Cellar/dnsmasq/2.68/dnsmasq.conf.example /usr/local/etc/dnsmasq.conf
+echo "address=/$1.dev/$2" >> /usr/local/etc/dnsmasq.conf
 # Make sure network uses the new nameserver
 sudo mkdir /etc/resolver
-sudo touch /etc/resolver/dsdev
-sudo echo "nameserver 127.0.0.1" > /etc/resolver/dsdev
+sudo touch /etc/resolver/dev
+#not sure why, but you need to check this file after finishing to make sure you have the right content
+sudo echo "nameserver 127.0.0.1" > /etc/resolver/dev
 
 # Make it load at start
 echo ":: Registering dnsmasq as a startup daemon..
 Your sudo password will be required."
-sudo cp -fv /usr/local/opt/dnsmasq/*.plist /Library/LaunchDaemons
+sudo cp -fv /usr/local/Cellar/dnsmasq/2.68/*.plist /Library/LaunchDaemons
 # We will unload it, in case it was already installed and enabled.
 sudo launchctl unload /Library/LaunchDaemons/homebrew.mxcl.dnsmasq.plist
 sudo launchctl load /Library/LaunchDaemons/homebrew.mxcl.dnsmasq.plist
